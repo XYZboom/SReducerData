1a5e910e998434c0a49789f39b4b7240d546b85f
xyzboom <xyzboom@qq.com>
2025-11-06 23:49:51 +0800

    GROOVY-11550

---
+++ GROOVY-11550-1/perses_result/main.groovy
@@ -0,0 +1,9 @@
+interface TgqifT<T0> {
+    T0 cbnQTZ(T0 pbk6NNJh, Object itFN, Object jQBI);
+}
+interface OkCro extends TgqifT<Void> {
+    default Object cbnQTZ(Object pbk6NNJh, Object itFN, Object jQBI) {
+    }
+}
+class C1k9095 implements OkCro {
+}
