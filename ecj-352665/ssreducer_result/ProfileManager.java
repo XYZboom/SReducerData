6775fed244c3aa8ce59cd443fa1099c41e6358ef
xyzboom <xyzboom@qq.com>
2025-11-29 14:41:25 +0800

    add ecj-352665

---
+++ ecj-352665/ssreducer_result/ProfileManager.java
@@ -0,0 +1,26 @@
+
+
+import org.w3c.dom.NodeList;
+import java.io.File;
+
+public abstract class ProfileManager
+{
+	
+	static{
+		if(!((File) null).exists()){}
+		else{
+			for(; 1 < ((NodeList) null).getLength(); ){
+				new ProfileManager.ProfileInfo();
+				}
+		}
+	}
+
+	/*public static Profile loadProfile(String name){
+		
+	}*/
+	
+	private class ProfileInfo
+	{
+		
+		}
+}
