cf085dcd565686a70fd2f660a724b556ef04a1ed
xyzboom <xyzboom@qq.com>
2025-11-29 20:27:42 +0800

    add JDK-8068399

---
+++ JDK-8068399/perses_result/Main.java
@@ -0,0 +1,11 @@
+import java.util.stream.*;
+       class Main {
+                  <T>T maxKey(Stream<T> stream,          T          function) {
+    }
+              int getXByFrequency(double frequency) {
+    }
+               {
+        int maxBpmIndex    ;
+        int xcur = getXByFrequency(maxKey(IntStream.range(0, maxBpmIndex).boxed(), i ->      abs                         ));
+    }
+}
---
+++ JDK-8068399/ssreducer_result/Main.java
@@ -0,0 +1,21 @@
+import java.util.function.Function;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+public class Main {
+
+    public static class Ref {
+
+        }
+
+    public static <T>T maxKey(Stream<T> stream, Function<T, Double> function) {
+
+        }
+
+    protected int getXByFrequency(double frequency) {
+        }
+
+    void foo() {
+        getXByFrequency(maxKey(IntStream.range(0, 1).boxed(), i -> Math.abs(((double[]) null)[(java.lang.Integer) null])));
+    }
+}
