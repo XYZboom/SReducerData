f2098f801943b50e72e5ffdef452ad5e00a2b6d0
xyzboom <xyzboom@qq.com>
2025-11-30 14:53:41 +0800

    update JDK-8068399

--- JDK-8068399/ssreducer_result/Main.java
+++ JDK-8068399/ssreducer_result/Main.java
@@ -4,18 +4,16 @@ import java.util.stream.Stream;
 
 public class Main {
 
-    public static class Ref {
-
-        }
-
     public static <T>T maxKey(Stream<T> stream, Function<T, Double> function) {
 
-        }
+        return null;
+    }
 
     protected int getXByFrequency(double frequency) {
-        }
+        return 1;
+    }
 
     void foo() {
-        getXByFrequency(maxKey(IntStream.range(0, 1).boxed(), i -> Math.abs(((double[]) null)[(java.lang.Integer) null])));
+        getXByFrequency(maxKey(IntStream.range(0, 1).boxed(), i -> Math.abs(((double[]) null)[i])));
     }
 }
